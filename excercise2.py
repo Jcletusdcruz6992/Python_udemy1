6ace2d53240acd393796a3965304c5b311148709
James Cletus Dcruz <jamescletusdcruz@gmail.com>
2022-03-28 00:31:19 -0400

    2022-03-27-12:30 am

---
+++ excercise2.py
@@ -0,0 +1,176 @@
+#####################################
+#### PART 9: FUNCTION EXERCISES #####
+#####################################
+
+
+# Complete the tasks below by writing functions! Keep in mind, these can be
+# really tough, its all about breaking the problem down into smaller, logical
+# steps. If you get stuck, don't feel bad about having to peek to the solutions!
+
+#####################
+## -- PROBLEM 1 -- ##
+#####################
+
+# Given a list of integers, return True if the sequence of numbers 1, 2, 3
+# appears in the list somewhere.
+
+# For example:
+
+
+#→ True
+# arrayCheck([1, 1, 2, 4, 1]) → False
+# arrayCheck([1, 1, 2, 1, 2, 3]) → True
+
+def arrayCheck(nums):
+    nums.sort()
+    i=0
+    j=0
+    k=0
+    for x in nums:
+        if(x==1):
+            i=i+1
+        elif(x==2):
+            j=j+1
+        elif(x==3):
+            k=k+1
+        else:
+            n=0
+    if(i>0 and k>0 and j>0):
+        return True
+    else:
+        return False
+
+
+
+    # CODE GOES HERE
+print(arrayCheck([1, 1, 2, 3, 1]))
+print(arrayCheck([1, 1, 2, 4, 1]))
+print(arrayCheck([1, 1, 2, 1, 2, 3]))
+#####################
+## -- PROBLEM 2 -- ##
+#####################
+
+# Given a string, return a new string made of every other character starting
+# with the first, so "Hello" yields "Hlo".
+
+# For example:
+
+# stringBits('Hello') → 'Hlo'
+# stringBits('Hi') → 'H'
+# stringBits('Heeololeo') → 'Hello'
+
+def stringBits(str):
+    new_str=set(str)
+    return new_str
+  # CODE GOES HERE
+new_str=str(stringBits('Hello'))
+y=str(stringBits('Hi'))
+z=str(stringBits('Heeololeo'))
+
+print(''.join(new_str))
+print(''.join(y))
+print(''.join(z))
+
+#####################
+## -- PROBLEM 3 -- ##
+#####################
+
+# Given two strings, return True if either of the strings appears at the very end
+# of the other string, ignoring upper/lower case differences (in other words, the
+# computation should not be "case sensitive").
+#
+# Note: s.lower() returns the lowercase version of a string.
+#
+# Examples:
+#
+# end_other('Hiabc', 'abc') → True
+# end_other('AbC', 'HiaBc') → True
+# end_other('abc', 'abXabc') → True
+
+
+def end_other(a, b):
+    a=a.lower()
+    b=b.lower()
+    if(a[(len(a)-3):]==b[(len(b)-3):] ):
+        return True
+    else:
+        return False
+print(end_other('Hiabc', 'abc'))
+print(end_other('AbC', 'HiaBc'))
+print(end_other('abc', 'abXabc'))
+print(end_other('ab', 'abXabc'))
+  # CODE GOES HERE
+
+#####################
+## -- PROBLEM 4 -- ##
+#####################
+
+# Given a string, return a string where for every char in the original,
+# there are two chars.
+
+# doubleChar('The') → 'TThhee'
+# doubleChar('AAbb') → 'AAAAbbbb'
+# doubleChar('Hi-There') → 'HHii--TThheerree'
+
+def doubleChar(str):
+    return (str+str)
+  # CODE GOES HERE
+print(''.join(sorted(doubleChar('The'))))
+print(''.join(sorted(doubleChar('AAbb'))))
+print(''.join(sorted(doubleChar('Hi-There'))))
+#####################
+## -- PROBLEM 5 -- ##
+#####################
+
+# Read this problem statement carefully!
+
+# Given 3 int values, a b c, return their sum. However, if any of the values is a
+# teen -- in the range 13-19 inclusive -- then that value counts as 0, except 15
+# and 16 do not count as a teens. Write a separate helper "def fix_teen(n):"that
+# takes in an int value and returns that value fixed for the teen rule.
+#
+# In this way, you avoid repeating the teen code 3 times (i.e. "decomposition").
+# Define the helper below and at the same indent level as the main no_teen_sum().
+# Again, you will have two functions for this problem!
+#
+# Examples:
+#
+# no_teen_sum(1, 2, 3) → 6
+# no_teen_sum(2, 13, 1) → 3
+# no_teen_sum(2, 1, 14) → 3
+
+def no_teen_sum(a, b, c):
+    return(fix_teen(a)+fix_teen(b)+fix_teen(c))
+  # CODE GOES HERE
+def fix_teen(n):
+    if(n>=13 and n<=19 and n!=15 and n!=16):
+        n=0
+        return n
+    else:
+        return n
+  # CODE GOES HERE
+print(no_teen_sum(1, 2, 3))
+print(no_teen_sum(2, 13, 1))
+print(no_teen_sum(2, 1, 14))
+#####################
+## -- PROBLEM 6 -- ##
+#####################
+
+# Return the number of even integers in the given array.
+#
+# Examples:
+#
+# count_evens([2, 1, 2, 3, 4]) → 3
+# count_evens([2, 2, 0]) → 3
+# count_evens([1, 3, 5]) → 0
+
+def count_evens(nums):
+    i=0
+    for x in nums:
+        if(x%2==0):
+            i=i+1
+    return i
+  # CODE GOES HERE
+print(count_evens([2, 1, 2, 3, 4]))
+print(count_evens([2, 2, 0]))
+print(count_evens([1, 3, 5]))
--- function.py
+++ function.py
@@ -14,3 +14,21 @@ def add_num(num1,num2):
         return 'Plase enter integer inputs'
 res=add_num(16,3)
 print(res)
+
+
+my_list=[1,2,3,4,5,6,7,8,9,10]
+
+def even(num):
+    return num%2==0
+nums=filter(even,my_list)
+print(list(nums))
+nume=filter(lambda numer:numer%2==0,my_list)
+print(list(nume))
+
+
+tweet='Go Sports, #sports'
+res=tweet.split('#')[1]
+print(res)
+
+print('x' in [1,2,3])
+print('x' in [1,2,3,'x'])
